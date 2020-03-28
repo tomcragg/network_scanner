40121463659321a62130ffabef45ce35a7c25db4
Tom Cragg <tom.cragg1@gmail.com>
2020-03-28 09:07:56 -0400

    Start of second algo

--- network_scanner.py
+++ network_scanner.py
@@ -3,7 +3,10 @@
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
-
+    #create instance of scapy ARP object
+    arp_request = scapy.ARP(pdst=ip)
+    print(arp_request.summary())
+    #print a list of fields that can be changed
+    #scapy.ls(scapy.ARP())
 
 scan("10.0.2.1/24")
